9f3faae78035b88a03a2001466bac26058aaa759
Professor <altopallaj21@gmail.com>
2026-02-26 18:39:39 +0100

    success handler implemented

--- src/main/java/com/albert/api_file/security/OAuth2SuccessHandler.java
+++ src/main/java/com/albert/api_file/security/OAuth2SuccessHandler.java
@@ -1,4 +1,109 @@
 package com.albert.api_file.security;
 
-public class OAuth2SuccessHandler {
+import com.albert.api_file.services.UserService;
+import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+    private final UserService userService;
+    private final OAuth2AuthorizedClientRepository authorizedClientRepository;
+    private final WebClient webClient = WebClient
+            .builder()
+            .baseUrl("https://api.github.com")
+            .build();
+
+    @Override
+    public void onAuthenticationSuccess(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull Authentication authentication
+    ) throws IOException {
+        var oauth2Token = (OAuth2AuthenticationToken) autentication;
+
+        var oidcProvider = oauth2Token.getAuthoritiesClientRegistrationId();
+        var oidcId = oauth2Token.getName();
+
+        System.out.println("Provider: " + oidcProvider);
+        System.out.println("Id: " + oidcId);
+
+        var user = userService.getUserByOidc(oidcId, oidcProvider);
+        System.out.println("User exists: " + user.isPresent());
+        if (user.isEmpty()) {
+            var createdUser = createUser(oidcId, oidcProvider, request, authentication);
+            if (createdUser == null) {
+                response.getWriter().println("Failed to create account");
+            } else {
+                response.getWriter().println("Registered account: " + createdUser.getUsername());
+            }
+        } else {
+            response.getWriter().println("Logged in as: " + user.get().getUsername);
+        }
+    }
+
+    @Nullable
+    private User createUser(
+            String oidcId,
+            String oidcProvider,
+            HttpServletRequest request,
+            Authentication authentication
+    ) {
+        var authorizedClient = authorizedClientRepository.loadAuthorizedClient(
+                oidcProvider,
+                authentication,
+                request
+        );
+
+        var accessToken = authorizedClient.getAccessToken().getTokenValue();
+
+        var emailResponse = webClient
+                .get()
+                .uri("/user/emails")
+                .header("Authorization", "Bearer " + accessToken)
+                .retrieve()
+                .toEntity(new ParameterizedTypeReference<GetEmailResponse>() {
+                })
+                .block();
+
+        if (emailResponse == null) {
+            return null;
+        }
+
+        if (emailResponse.getStatusCode() != HttpStatus.OK) {
+            return null;
+        }
+
+        var emails = emailResponse.getBody();
+        if (emails = null || emails.isEmpty()) {
+            return null;
+        }
+
+        var email = emails.getFirst();
+
+        return userService.createOidcUser(email.getEmail(), oidcId, oidcProvider);
+    }
+
+    @Getter
+    @Setter
+    private static class GetEmailsResponse {
+        private String email;
+    }
+
 }
