337ba60b2648eaa09286278101cad825d2ed29cd
Professor <altopallaj21@gmail.com>
2026-03-02 02:34:12 +0100

    added HATEOAS to FolderResponse

--- src/main/java/com/albert/api_file/dtos/FolderResponse.java
+++ src/main/java/com/albert/api_file/dtos/FolderResponse.java
@@ -1,19 +1,24 @@
 package com.albert.api_file.dtos;
 
+import com.albert.api_file.controllers.FolderController;
 import com.albert.api_file.models.File;
 import com.albert.api_file.models.Folder;
 import com.albert.api_file.models.User;
 import com.albert.api_file.utilites.DateFormatterUtility;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Getter
 @Setter
-public class FolderResponse {
+public class FolderResponse extends RepresentationModel<FolderResponse> {
 
     private final UUID id;
     private String name;
@@ -33,11 +38,16 @@ public class FolderResponse {
                 .map(FileResponse::fromModel)
                 .toList();
 
-        return new FolderResponse(
+        FolderResponse response = new FolderResponse(
                 folder.getId(),
                 folder.getName(),
                 fileResponses,
                 folder.getOwner().getUsername()
         );
+
+        response.add(linkTo(methodOn(FolderController.class).getFolderById(folder.getId(), null))
+                .withSelfRel());
+
+        return response;
     }
 }
