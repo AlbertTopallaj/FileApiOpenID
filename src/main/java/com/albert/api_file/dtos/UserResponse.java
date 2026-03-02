a8fc69efd4cd28b25a4b4cf5b73a9d92af330930
Professor <altopallaj21@gmail.com>
2026-03-02 02:50:23 +0100

    added HATEOAS to UserResponse

--- src/main/java/com/albert/api_file/dtos/UserResponse.java
+++ src/main/java/com/albert/api_file/dtos/UserResponse.java
@@ -1,16 +1,21 @@
 package com.albert.api_file.dtos;
 
+import com.albert.api_file.controllers.UserController;
 import com.albert.api_file.models.User;
 import com.albert.api_file.utilites.DateFormatterUtility;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.util.Date;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Getter
 @Setter
-public class UserResponse {
+public class UserResponse extends RepresentationModel<UserResponse> {
 
     private final UUID id;
     private String username;
@@ -23,10 +28,15 @@ public class UserResponse {
     }
 
     public static UserResponse fromModel(User user) {
-        return new UserResponse(
+        UserResponse response = new UserResponse(
                 user.getId(),
                 user.getUsername(),
                 user.getCreatedAt().format(DateFormatterUtility.DATE_TIME_FORMATTER)
         );
+
+        response.add(linkTo(methodOn(UserController.class).getUserById(user.getId()))
+                .withSelfRel());
+
+        return response;
     }
 }
